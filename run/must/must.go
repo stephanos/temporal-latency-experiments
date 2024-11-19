b2b8ea08f44709e5565ec70a3d40065492c38f64
Stephan Behnke <stephan.behnke@temporal.io>
2024-11-18 20:34:16 -0800

    adjust

--- run/must/must.go
+++ run/must/must.go
@@ -1,14 +1,19 @@
 package must
 
+import (
+	"github.com/antithesishq/antithesis-sdk-go/assert"
+)
+
 func Must1(err error) {
 	if err != nil {
+		assert.Unreachable("[WKL] Update benchmark failed", map[string]any{"err": err.Error()})
 		panic(err)
 	}
 }
 
 func Must[T any](t T, err error) T {
 	if err != nil {
-		panic(err)
+		assert.Unreachable("[WKL] Update benchmark failed", map[string]any{"err": err.Error()})
 	}
 	return t
 }
